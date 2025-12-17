b6872026a86f59516b9b8fe7b0eb5b604c4dd475
Christian Hinkle <christianhinko@gmail.com>
2025-12-17 11:29:17 -0700

    [cpp][fix] Regenerate type ids to be unique from the O3deUtils_Core gem.

--- Code/Include/O3deUtils_Misc/O3deUtils_MiscTypeIds.h
+++ Code/Include/O3deUtils_Misc/O3deUtils_MiscTypeIds.h
@@ -4,16 +4,16 @@
 namespace O3deUtils_Misc
 {
     // System Component TypeIds
-    inline constexpr const char* O3deUtils_MiscSystemComponentTypeId = "{BB24F523-1FD0-4FA1-A862-3126458F2740}";
-    inline constexpr const char* O3deUtils_MiscEditorSystemComponentTypeId = "{5B34CF7C-0D57-462D-A027-0E55ACB36302}";
+    inline constexpr const char* O3deUtils_MiscSystemComponentTypeId = "{F2EB846E-5FFC-4275-B66C-EB70DC2A7FE7}";
+    inline constexpr const char* O3deUtils_MiscEditorSystemComponentTypeId = "{434E7C84-D770-4641-85BE-D1A2E98B4648}";
 
     // Module derived classes TypeIds
-    inline constexpr const char* O3deUtils_MiscModuleInterfaceTypeId = "{EEBFEC30-68DB-47FA-BE3C-7D477234A6DD}";
-    inline constexpr const char* O3deUtils_MiscModuleTypeId = "{91EFF5D4-0317-4272-9D19-F78E2D5DC49D}";
+    inline constexpr const char* O3deUtils_MiscModuleInterfaceTypeId = "{8B8E4566-F3B0-4165-ADAE-6FFA9BD0F8E9}";
+    inline constexpr const char* O3deUtils_MiscModuleTypeId = "{E76A6970-3368-4A13-A0A5-A74C68068FB2}";
     // The Editor Module by default is mutually exclusive with the Client Module
     // so they use the Same TypeId
     inline constexpr const char* O3deUtils_MiscEditorModuleTypeId = O3deUtils_MiscModuleTypeId;
 
     // Interface TypeIds
-    inline constexpr const char* O3deUtils_MiscRequestsTypeId = "{5738B9F5-8ADC-40C6-9E6D-65748A109218}";
+    inline constexpr const char* O3deUtils_MiscRequestsTypeId = "{C6D634C3-B071-4A02-A40E-B93248634B62}";
 } // namespace O3deUtils_Misc
