3ffcecd78c24e59f8b44455f6af8c4abd7d06e84
Christian Hinkle <christianthinkle@gmail.com>
2026-01-15 23:23:51 -0700

    [fix][build][engine] Fix linker errors from "Tools" module target depending on non-tools shared library, by adding importing/exporting to required classes used by the "Tools" target.

--- Code/Source/O3deUtils_MiscModuleInterface.h
+++ Code/Source/O3deUtils_MiscModuleInterface.h
@@ -3,14 +3,15 @@
 #include <AzCore/Module/Module.h>
 #include <AzCore/RTTI/RTTIMacros.h>
 #include <AzCore/RTTI/TypeInfoSimple.h>
+#include <O3deUtils_Misc/O3deUtils_MiscConfiguration.h>
 
 namespace O3deUtils_Misc
 {
-    class O3deUtils_MiscModuleInterface
+    class O3DEUTILS_MISC_API O3deUtils_MiscModuleInterface
         : public AZ::Module
     {
     public:
-        AZ_TYPE_INFO_WITH_NAME_DECL(O3deUtils_MiscModuleInterface)
+        AZ_TYPE_INFO_WITH_NAME_DECL_API(O3DEUTILS_MISC_API, O3deUtils_MiscModuleInterface)
         AZ_RTTI_NO_TYPE_INFO_DECL()
         AZ_CLASS_ALLOCATOR_DECL
 
--- Code/Source/O3deUtils_MiscSystemComponent.h
+++ Code/Source/O3deUtils_MiscSystemComponent.h
@@ -4,16 +4,22 @@
 #include <AzCore/Component/Component.h>
 #include <AzCore/Component/TickBus.h>
 #include <O3deUtils_Misc/O3deUtils_MiscBus.h>
+#include <O3deUtils_Misc/O3deUtils_MiscConfiguration.h>
 
 namespace O3deUtils_Misc
 {
-    class O3deUtils_MiscSystemComponent
+    class O3DEUTILS_MISC_API O3deUtils_MiscSystemComponent
         : public AZ::Component
         , protected O3deUtils_MiscRequestBus::Handler
         , public AZ::TickBus::Handler
     {
     public:
-        AZ_COMPONENT_DECL(O3deUtils_MiscSystemComponent);
+        //
+        // @Christian: Note: [engine] This uses the `AZ_COMPONENT_DECL_API` macro from custom engine fork: https://github.com/ChristianHinkle/o3de/tree/support-for-az-components-in-shared-libraries.
+        //
+        // I'm opening a pull request, and hopefully it eventually gets merged into the actual engine.
+        //
+        AZ_COMPONENT_DECL_API(O3DEUTILS_MISC_API, O3deUtils_MiscSystemComponent);
 
         static void Reflect(AZ::ReflectContext* context);
 
