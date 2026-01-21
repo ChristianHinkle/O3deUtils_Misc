e2ab69f25c088820f585d11b541697912f55d14e
Christian Hinkle <christianthinkle@gmail.com>
2026-01-21 06:33:44 -0700

    [cmake][auto-gen][todo][comment] Delete usage of multiplayer gem AutoGen and auto-components, and write todo comments explaining what we would have to do in order to use auto-components in this gem.

--- Code/o3deutils_misc_common_autogen_files.cmake
+++ Code/o3deutils_misc_common_autogen_files.cmake
@@ -2,20 +2,9 @@
 # It's our job to define this cmake variable which the `ly_include_cmake_file_list` function looks at after this file is done being processed.
 set(FILES)
 
+# @Christian TODO: [todo][cmake][auto-gen] List out the required multiplayer AutoGen files here if we want
+# to make multiplayer auto-components. Last time I tried this, it was a bit tricky because (1) I'd have to make
+# the system component call `RegisterMultiplayerComponents` and we'd have to decide on whether we want the editor
+# target to also do that or not, and (2) I was getting compiler errors because it seems like it would try to
+# start building the C++ before the AutoGen custom command was run for certain targets, but Idk exactly.
 # Append required multiplayer gem files.
-block(SCOPE_FOR VARIABLES PROPAGATE FILES)
-    get_property(multiplayer_gem_root GLOBAL PROPERTY "@GEMROOT:Multiplayer@")
-
-    list(APPEND FILES
-        ${multiplayer_gem_root}/Code/Include/Multiplayer/AutoGen/AutoComponent_Common.jinja
-        ${multiplayer_gem_root}/Code/Include/Multiplayer/AutoGen/AutoComponent_Header.jinja
-        ${multiplayer_gem_root}/Code/Include/Multiplayer/AutoGen/AutoComponent_Source.jinja
-        ${multiplayer_gem_root}/Code/Include/Multiplayer/AutoGen/AutoComponentTypes_Header.jinja
-        ${multiplayer_gem_root}/Code/Include/Multiplayer/AutoGen/AutoComponentTypes_Source.jinja
-    )
-endblock()
-
-# Append our AutoGen xml files.
-list(APPEND FILES
-    Source/AutoGen/MyPlaceholderAutoComponent.AutoComponent.xml
-)
