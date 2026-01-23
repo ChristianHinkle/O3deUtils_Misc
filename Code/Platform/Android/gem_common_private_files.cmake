b87c57b567cb864ed1b7fc97262f3efc7ef590e9
Christian Hinkle <christianthinkle@gmail.com>
2026-01-22 17:45:12 -0700

    [cmake] Eliminate some unnecessary usage of the gem name in cmake code, to make the code more portable.

--- Code/Platform/Android/gem_common_private_files.cmake
+++ Code/Platform/Android/gem_common_private_files.cmake
@@ -1,8 +1,3 @@
 
-# Platform specific files for Android
-# i.e. ../Source/Android/O3deUtils_MiscAndroid.cpp
-#      ../Source/Android/O3deUtils_MiscAndroid.h
-#      ../Include/Android/O3deUtils_MiscAndroid.h
-
 set(FILES
 )
--- Code/Platform/Linux/gem_common_private_files.cmake
+++ Code/Platform/Linux/gem_common_private_files.cmake
@@ -1,8 +1,3 @@
 
-# Platform specific files for Linux
-# i.e. ../Source/Linux/O3deUtils_MiscLinux.cpp
-#      ../Source/Linux/O3deUtils_MiscLinux.h
-#      ../Include/Linux/O3deUtils_MiscLinux.h
-
 set(FILES
 )
